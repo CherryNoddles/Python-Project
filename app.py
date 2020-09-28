400a878312160e2629118d06177fa72e374f56c1
CherryNoddles <69775709+CherryNoddles@users.noreply.github.com>
2020-09-28 09:12:49 +0800

    Update app.py
    Changed code to MCQ Revision Program.

--- app.py
+++ app.py
@@ -1,57 +1,127 @@
 
-print("Title of program: Encouragement bot")
+print("Title of program: MCQ revision program")
 print()
-while True:
-  description = input("Could you describe how you feel in a sentence?")
 ​
-  list_of_words = description.split()
+counter = 0
+score = 0
+total_num_of_qn = 3
 ​
-  feelings_list = []
-  encouragement_list = []
-  counter = 0
+​
+counter +=1
+tracker = 0
+​
+while tracker !=1:
   
-  for each_word in list_of_words:
-    
-    if each_word == "sad":
-      feelings_list.append("sad")
-      encouragement_list.append("tomorrow will be a better day")
-      counter += 1
-    if each_word == "happy":
-      feelings_list.append("happy")
-      encouragement_list.append("to keep smiling")
-      counter += 1
-    if each_word == "tired":
-      feelings_list.append("tired")
-      encouragement_list.append("you are stronger than you think")
-    if each_word == "stressed":
-      feelings_list.append("stressed")
-      encouragement_list.append("you will get through this")
-      counter += 1
-​
-  if counter == 0:
-    
-      output = "Sorry I don't really understand. Please use different words?"
-​
-  elif counter == 1:
-    
-      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
-​
+  print("Q"+str(counter)+") "+ "What is 12 + 2?")
+  print("   a) 10")
+  print("   b) 24")
+  print("   c) 14")
+  print("   d) 6")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. This is not subtraction."
+    score -=1
+  elif answer == "b":
+    output = "Wrong. This is not multiplication."
+    score -=1
+  elif answer == "c":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "d":
+    output = "Wrong. This is not division."
+    score -=1
   else:
+    output = "Please choose a, b, c or d only."
+  
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
 ​
-    feelings = ""    
-    for i in range(len(feelings_list)-1):
-      feelings += feelings_list[i] + ", "
-    feelings += "and " + feelings_list[-1]
+​
+counter +=1
+tracker = 0
+​
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "The chemical formula H2 represents")
+  print("   a) one hydrogen molecule")
+  print("   b) two hydrogen atoms")
+  print("   c) one hydrogen atom")
+  print("   d) two hydrogen molecules")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "b":
+    output = "Wrong. If so, then it will be written as H and H - two hydrogen atoms."
+    score -=1
+  elif answer == "c":
+    output = "Wrong. Clearly the number 2 in the formulae must mean something?"
+    score -=1
     
-    encouragement = ""    
-    for j in range(len(encouragement_list)-1):
-      encouragement += encouragement_list[i] + ", "
-    encouragement += "and " + encouragement_list[-1]
-​
-    output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
+  elif answer == "d":
+    output = "Wrong. What's the difference between a molecule and an atom?"
+    score -=1
+  else:
+    output = "Please choose a, b, c or d only."
 ​
   print()
   print(output)
   print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+  
 ​
+counter +=1
+tracker = 0
+​
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "Which represents the electronic configuration of a non-metal?")
+  print("   a) 2,1")
+  print("   b) 2,8,3")
+  print("   c) 2,8,8,2")
+  print("   d) 2,7")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+  elif answer == "b":
+    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+  elif answer == "c":
+    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+    
+  elif answer == "d":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  else:
+    output = "Please choose a, b, c or d only."
+​
+  
+​
+  print()
+  print(output.lower())
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+print("End of quiz!)
+  
+​
+
 
